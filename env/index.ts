554465647ed527a5870b1d9a53000f57d6110707
Rodrigo Spinelli <rodrigo.spinelli9@gmail.com>
2025-05-08 20:39:43 -0300

    feat: add environment variable validation using zod

---
+++ env/index.ts
@@ -0,0 +1,19 @@
+import dotenv from "dotenv";
+import { z } from "zod";
+
+dotenv.config();
+
+const envSchema = z.object({
+  PORT: z.coerce.number().default(3000),
+  NODE_ENV: z.enum(["dev", "test", "production"]).default("dev"),
+});
+
+const _env = envSchema.safeParse(process.env);
+
+if (_env.success === false) {
+  console.error("❌ Invalid environment variables", _env.error.format());
+
+  throw new Error("Invalid environment variables.");
+}
+
+export const env = _env.data;
