84a0a2d5d59fce7db1665dc704bb46f1bb62a3f2
Rodrigo Spinelli <rodrigo.spinelli9@gmail.com>
2025-05-08 21:01:56 -0300

    refactor: update import paths to use alias for app and env modules

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,5 @@
-import { app } from "./app";
-import { env } from "../env";
+import { app } from "@/app";
+import { env } from "@/env";
 
 app.listen({ port: env.PORT, host: "0.0.0.0" }, () => {
   console.log(`🚀 Server is running on port ${env.PORT}`);
