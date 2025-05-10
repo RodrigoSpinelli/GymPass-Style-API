ceb22dbbad4a438e652dc952a78c2733398ebed6
Rodrigo Spinelli <rodrigo.spinelli9@gmail.com>
2025-05-10 19:42:24 -0300

    feat: implement InMemoryUsersRepository for user management

---
+++ src/http/repositories/in-memory/users-repositorys.ts
@@ -0,0 +1,25 @@
+import { Prisma, User } from "@prisma";
+import { UsersRepository } from "../users-repositoy";
+
+export class InMemoryUsersRepository implements UsersRepository {
+  public items: User[] = [];
+
+  async create(data: Prisma.UserCreateInput): Promise<User> {
+    const user = {
+      id: "1",
+      name: data.name,
+      email: data.email,
+      password_hash: data.password_hash,
+      created_at: new Date(),
+    };
+
+    this.items.push(user);
+
+    return user;
+  }
+  async findByEmail(email: string): Promise<User | null> {
+    const user = this.items.find((user) => user.email === email);
+
+    return user ?? null;
+  }
+}
