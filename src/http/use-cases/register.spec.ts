7bb104c16f9bba32ba406922336607b2dd343020
Rodrigo Spinelli <rodrigo.spinelli9@gmail.com>
2025-05-10 19:42:15 -0300

    test: add unit tests for RegisterUseCase to validate user registration functionality

---
+++ src/http/use-cases/register.spec.ts
@@ -0,0 +1,56 @@
+import { it, expect, describe } from "vitest";
+import { InMemoryUsersRepository } from "../repositories/in-memory/users-repositorys";
+import { RegisterUseCase } from "./register";
+import { compare } from "bcryptjs";
+import { UserAlreadyExistsError } from "./error/user-already-exists-error";
+describe("Register use case", () => {
+  it("should be able to register", async () => {
+    const inMemoryUsersRepository = new InMemoryUsersRepository();
+    const registerUseCase = new RegisterUseCase(inMemoryUsersRepository);
+
+    const { user } = await registerUseCase.execute({
+      name: "John Doe",
+      email: "john.doe@example.com",
+      password: "123456",
+    });
+
+    expect(user.id).toEqual(expect.any(String));
+  });
+
+  it("should hash user password upon registration", async () => {
+    const inMemoryUsersRepository = new InMemoryUsersRepository();
+    const registerUseCase = new RegisterUseCase(inMemoryUsersRepository);
+
+    const { user } = await registerUseCase.execute({
+      name: "John Doe",
+      email: "john.doe@example.com",
+      password: "123456",
+    });
+
+    const isPasswordCorrectlyHashed = await compare(
+      "123456",
+      user.password_hash
+    );
+
+    expect(isPasswordCorrectlyHashed).toBe(true);
+  });
+
+  it("should not be able to register with same email twice", async () => {
+    const inMemoryUsersRepository = new InMemoryUsersRepository();
+    const registerUseCase = new RegisterUseCase(inMemoryUsersRepository);
+
+    await registerUseCase.execute({
+      name: "John Doe",
+      email: "john.doe@example.com",
+      password: "123456",
+    });
+
+    await expect(
+      registerUseCase.execute({
+        name: "John Doe",
+        email: "john.doe@example.com",
+        password: "123456",
+      })
+    ).rejects.toBeInstanceOf(UserAlreadyExistsError);
+  });
+});
