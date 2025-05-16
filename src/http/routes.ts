44fba1239c5bb6f8ed97a159b6e7a466b5dff961
Rodrigo Spinelli <rodrigo.spinelli9@gmail.com>
2025-05-16 00:01:06 -0300

    feat: add profile route and JWT verification middleware for authenticated access

--- src/http/routes.ts
+++ src/http/routes.ts
@@ -1,8 +1,13 @@
 import { RegisterController } from "@/http/controllers/register";
 import { FastifyInstance } from "fastify";
 import { AuthenticateController } from "./controllers/authenticate";
+import { ProfileController } from "./controllers/profile";
+import { verifyJwt } from "./middlewares/verify-jwt";
 
 export const appRoutes = async (app: FastifyInstance) => {
   app.post("/users", RegisterController);
   app.post("/sessions", AuthenticateController);
+
+  // Authenticated
+  app.get("/me", { onRequest: [verifyJwt] }, ProfileController);
 };
