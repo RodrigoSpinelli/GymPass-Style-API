870d66e8688c9505cdbf664908d5de277c7db03d
Rodrigo Spinelli <rodrigo.spinelli9@gmail.com>
2025-05-16 19:29:24 -0300

    feat: enhance user creation function to include role assignment and password hashing

--- src/utils/test/create-and-authenticate-user.ts
+++ src/utils/test/create-and-authenticate-user.ts
@@ -1,18 +1,23 @@
 import request from "supertest";
 import { FastifyInstance } from "fastify";
-
-export async function createAndAuthenticateUser(app: FastifyInstance) {
-  await request(app.server).post("/users").send({
-    name: "John Doe",
-    email: "john.doe@example.com",
-    password: "password",
+import { prisma } from "@/lib/prisma";
+import { hash } from "bcryptjs";
+export async function createAndAuthenticateUser(app: FastifyInstance, isAdmin = false) {
+  const user = await prisma.user.create({
+    data: {
+      name: "John Doe",
+      email: "john.doe@example.com",
+      password_hash: await hash("123456", 6),
+      role: isAdmin ? "ADMIN" : "MEMBER",
+    },
   });
+
 
   const authenticateResponse = await request(app.server)
     .post("/sessions")
     .send({
       email: "john.doe@example.com",
-      password: "password",
+      password: "123456",
     });
 
   const { token } = authenticateResponse.body;
